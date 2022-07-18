dd058a48ffcc2aa873d2d9d49858b983c128d221
alok <alok60541@gmail.com>
2022-07-18 21:38:24 +0530

    create operation

--- index.js
+++ index.js
@@ -2,32 +2,33 @@ const mongoose = require('mongoose');
 
 
 // **** create/ saved in db model + schema *****
-// const saveInDb = async ()=>{
-//     await mongoose.connect("mongodb://localhost:27017/firstDatabase");
+const saveInDb = async ()=>{
+    await mongoose.connect("mongodb://localhost:27017/firstDatabase");
 
-//     //schema => All fields are defined under this. only specified field + data types data can be filled in database rest will be blocked
-//     const productSchema = new mongoose.Schema({
-//         name: String,
-//         price: Number,
-//         brand: String,
-//         category: String
-//     });
+    //schema => All fields are defined under this. only specified field + data types data can be filled in database rest will be blocked
+    const productSchema = new mongoose.Schema({
+        name: String,
+        price: Number,
+        brand: String,
+        category: String
+    });
 
-//     // model => by using schema , it connect mongodb to nodeJs
+    // model => by using schema , it connect mongodb to nodeJs
 
-//     const productModel = mongoose.model('products', productSchema);
+    const productModel = mongoose.model('products', productSchema);
 
-//     // let data = new productModel({name: "m10", price: 1000});
-//     let data = new productModel(
-//         {
-//             name: "Note NOte",
-//             price: 50,
-//             brand: "Temporary",
-//             category: "Rupee"
-//         });
+    // let data = new productModel({name: "m10", price: 1000});
+    let data = new productModel(
+        {
+            name: "Note NOte",
+            price: 50,
+            brand: "Temporary",
+            category: "Rupee"
+        });
 
-//     let result = await data.save();
-//     console.log(result);
-// }
+    let result = await data.save();
+    console.log(result);
+}
 
 // saveInDb();
+
