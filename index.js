67a1bc966155c644393b2a6fa38df6dfd88b9f19
alok <alok60541@gmail.com>
2022-07-18 21:35:59 +0530

    create operation

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const mongoose = require('mongoose');
 
-const main = async ()=>{
-    await mongoose.connect("mongodb://localhost:27017/firstDatabase");
 
-    //schema => All fields are defined under this. only specified field + data types data can be filled in database rest will be blocked
-    const productSchema = new mongoose.Schema({
-        name: String,
-        price: Number
-    });
+// **** create/ saved in db model + schema *****
+// const saveInDb = async ()=>{
+//     await mongoose.connect("mongodb://localhost:27017/firstDatabase");
 
-    // model => by using schema , it connect mongodb to nodeJs
+//     //schema => All fields are defined under this. only specified field + data types data can be filled in database rest will be blocked
+//     const productSchema = new mongoose.Schema({
+//         name: String,
+//         price: Number,
+//         brand: String,
+//         category: String
+//     });
 
-    const productModel = mongoose.model('products', productSchema);
+//     // model => by using schema , it connect mongodb to nodeJs
 
-    let data = new productModel({name: "m10", price: 1000});
-    let result = await data.save();
+//     const productModel = mongoose.model('products', productSchema);
 
-    console.log(result);
+//     // let data = new productModel({name: "m10", price: 1000});
+//     let data = new productModel(
+//         {
+//             name: "Note NOte",
+//             price: 50,
+//             brand: "Temporary",
+//             category: "Rupee"
+//         });
 
-}
+//     let result = await data.save();
+//     console.log(result);
+// }
 
-main();
+// saveInDb();
