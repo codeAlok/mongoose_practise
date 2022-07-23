a34496cad9f9bbab3b537eca50ee747d4f4e29f3
alok <alok60541@gmail.com>
2022-07-23 12:33:41 +0530

    insert data

--- mongoose_thp/app.js
+++ mongoose_thp/app.js
@@ -9,7 +9,7 @@ const playListSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
-    },   
+    },
     ctype: String,
     videos: Number,
     author: String,
@@ -22,8 +22,43 @@ const playListSchema = new mongoose.Schema({
 
 
 // ******** Model *****
-// collection creation
 /* Mongoose model provides an interface to the database for creating, querrying, updating, deleteing records.  etc
 */
 
+//collection creation
 const PlayList = new mongoose.model("playlist", playListSchema);
+
+
+// *** create document or insert ***
+// normally
+
+// const reactPlaylist = new PlayList({
+//     name: "React Js",   
+//     ctype: "Front End",
+//     videos: 40,
+//     author: "Alok Kumar",
+//     active: true,
+// });
+
+// reactPlaylist.save();  //takes time to save so, handle through async/await
+
+// **Through async/await **
+const createDocument = async () => {
+    try {
+        const reactPlaylist = new PlayList({
+            name: "React Js",
+            ctype: "Front End",
+            videos: 40,
+            author: "Alok Kumar",
+            active: true,
+        });
+
+        const result = await reactPlaylist.save();
+        console.log(result);
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
+
+createDocument();
